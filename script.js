7c8054940feba8a9302be5495af5beeafc9bc8ec
mirinas <augustas.mirinas@gmail.com>
2025-01-07 11:35:59 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// let a = 'Hello';
+// let b = 'Augustas';
+
+// alert(a + ' ' + b);
+
+
+let element = document.querySelector('#title');
+// fono spalva
+element.style.background = 'lightblue';
+// teksto spalva
+element.style.color = 'green';
